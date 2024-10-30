1da70f6bd17264397b5fd0ae2a550af5d3478a88
carmen <carmenmtz97@comunidad.unam.mx>
2024-10-30 16:34:55 -0600

    Actualizacion

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y hago proyectos de UI/UX')
+  .typeString('FRONT END | UI UX')
   .pauseFor(200)
   .deleteChars(10)
   .start();
